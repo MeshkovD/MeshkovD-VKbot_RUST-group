1423e5fe2064fc8b34201972e24f062dd383b12e
“Мешков <>
2021-10-15 23:38:27 +0700

    Изменил некоторые ответы бота. Рефакторинг

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-print('*' * 40)
-print("Server started")
-print('*' * 40)
-
-
 import vk_api
 import time
 
@@ -10,7 +5,6 @@ from vk_api.longpoll import VkLongPoll, VkEventType
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 
 from my_bot_token import token
-
 
 vk_session = vk_api.VkApi(token=token)
 vk_session._auth_token()
@@ -31,88 +25,55 @@ keyboard.add_line()  # Переход на новую строку
 keyboard.add_button('Бан\Разбан', color=VkKeyboardColor.NEGATIVE)
 keyboard.add_button('Команды', color=VkKeyboardColor.NEGATIVE)
 
-
 connection_counter = 0
 
+questions = {
+    'Когда вайп?': 'Вайп каждые 5 дней, дату последнего вайпа можно найти на стене сообщества или в нашем '
+                   'дискорд канале https://discord.gg/K2kWjvWKnB.',
+    'Промо': 'Промокод для новичков "new". Так же вы можете получить вознаграждения за проведенное время на сервере , '
+             'используйте команду /case в игре . Важно! Сначала авторизируйтесь в магазине  '
+             'https://galakrond.playrust.ru/ перед тем как забрать вознаграждение.',
+
+    'Бан\Разбан': 'Все зявки на разбан рассматриваются в этом топике https://vk.com/topic-185597153_40565047. '
+                  'Перед тем как написать заявку, рекомендуем сначала ознакомиться с правилами сервера '
+                  'https://vk.com/topic-185597153_40516485.',
+    'Команды': 'С доступными командами чата можно ознакомиться тут https://vk.com/@galakrondproject-komandy',
+    'Хочу стать модератором': 'https://vk.com/galakrondproject?w=app5708398_-185597153',
+    'Пиратка?': 'Нет, наш сервер для владельцев лицензии.',
+    'начать': 'Ок, начинаем!',
+    'начало': 'Ок, начинаем!',
+    'начинаем': 'Ок, начинаем!',
+    'start': 'Ок, начинаем!',
+    'старт': 'Ок, начинаем!',
+}
+
 try:
+    def send_reply(event):
+        if questions.get(event.text):
+            vk.messages.send(  # Отправляем сообщение
+                user_id=event.user_id,
+                message=questions.get(event.text),
+                keyboard=keyboard.get_keyboard(),
+                random_id=0
+            )
+
+
     def main():
-        global connection_counter
-        connection_counter = connection_counter +1
-        print(time.strftime("%H:%M:%S"), '* Попытка подключения №{num}' .format(num = connection_counter))
+
         try:
             longpoll = VkLongPoll(vk_session)
-            print(time.strftime("%H:%M:%S"), "* Успешное подключение")
-            print(time.strftime("%H:%M:%S"), "* Бот запущен")
-            print('*' * 40)
+
             for event in longpoll.listen():
-                if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
-                #Слушаем longpoll, если пришло сообщение то:
-                    if event.text == 'Когда вайп?': #Если написали заданную фразу
-                        if event.from_user: #Если написали в ЛС
-                            vk.messages.send( #Отправляем сообщение
-                                user_id=event.user_id,
-                                message='Вайп каждую пятницу в 10:00 по МСК.',
-                                keyboard=keyboard.get_keyboard(),
-                                random_id=0
-                            )
-                    elif event.text == 'Промо': #Если написали заданную фразу
-                        vk.messages.send( #Отправляем сообщение
-                                user_id=event.user_id,
-                                message='Промокод для новичков "new". Так же вы можете получить вознаграждения за проведенное время на сервере , используйте команду /case в игре . Важно! Сначала авторизируйтесь в магазине  https://galakrond.playrust.ru/ перед тем как забрать вознаграждение.',
-                                keyboard=keyboard.get_keyboard(),
-                                random_id=0
-                            )
-                    elif event.text == 'Бан\Разбан':
-                        vk.messages.send( #Отправляем сообщение
-                                user_id=event.user_id,
-                                message='Все зявки на разбан рассматриваются в этом топике https://vk.com/topic-185597153_40565047. Перед тем как написать заявку, рекомендуем сначала ознакомиться с правилами сервера https://vk.com/topic-185597153_40516485.',
-                                keyboard=keyboard.get_keyboard(),
-                                random_id=0
-                        )
-                    elif event.text == 'Команды':
-                        vk.messages.send( #Отправляем сообщение
-                                user_id=event.user_id,
-                                message='С доступными командами чата можно ознакомиться тут https://vk.com/@galakrondproject-komandy',
-                                keyboard=keyboard.get_keyboard(),
-                                random_id=0
-                        )
-                    elif event.text == 'Хочу стать модератором':
-                        vk.messages.send( #Отправляем сообщение
-                                user_id=event.user_id,
-                                message='https://vk.com/@galakrondproject-nabor-moderacii',
-                                keyboard=keyboard.get_keyboard(),
-                                random_id=0
-                        )
-                    elif event.text == 'Пиратка?':
-                        vk.messages.send(
-                                message='Нет, наш сервер для владельцев лицензии.',
-                                user_id=event.user_id,
-                                keyboard=keyboard.get_keyboard(),
-                                random_id=0
-                        )
-                    elif 'начать' in (event.text).lower() \
-                                or 'начало' in (event.text).lower() \
-                                or 'начинаем' in (event.text).lower()\
-                                or 'start' in (event.text).lower():
-                        vk.messages.send(  # Отправляем сообщение
-                                user_id=event.user_id,
-                                message='Ок, начинаем!',
-                                keyboard=keyboard.get_keyboard(),
-                                random_id=0
-                        )
+                if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:  # Слушаем longpoll, если пришло сообщение отвечаем
+                    send_reply(event)
 
         except Exception as ex:
-            print(time.strftime("%H:%M:%S"), "* Подключиться не удалось")
-            print(time.strftime("%H:%M:%S"),"* Причина:", ex)
+            print(ex)
         finally:
-
-            print(time.strftime("%H:%M:%S"), "* Ждём 5 секунд")
             time.sleep(5)
 
 except Exception as ex:
-    print('-' * 20)
-    print(time.ctime(), ex)
-    print('-' * 20)
+    print(ex)
 
 if __name__ == '__main__':
     while True:
